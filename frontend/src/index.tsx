a6f735c3ec6530fcdef9155331598b74e3326610
doctored11 <edit.shultz@yandex.ru>
2024-11-18 00:01:49 +0300

    try get tg Obj

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,25 @@
 import React, { createContext, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { OneHandSlotMashine } from "./pages/oneHabdSlotMashine/oneHandSlotMashine";
-import {  PlayerProvider } from "./PlayerContext";
+import { PlayerProvider } from "./PlayerContext";
 import "./index.css";
 import "./normalize.css";
 
+const tg = window.Telegram.WebApp;
 function App() {
   //todo - вынести игрока и работу с балансом
 
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  };
   return (
     <>
+      <button onClick={onClose}>ЗАкрыть</button>
       <PlayerProvider>
-      
         <OneHandSlotMashine />
       </PlayerProvider>
     </>
