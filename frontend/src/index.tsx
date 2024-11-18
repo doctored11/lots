45c0f5a7f74f7127590a925c3d6a1e9f00070cbd
doctored11 <edit.shultz@yandex.ru>
2024-11-19 00:31:47 +0300

    [hot test] get tg data n

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,18 +10,18 @@ const targetAddress = process.env.REACT_APP_TARGET_ADDRESS;
 function App() {
   //todo - вынести игрока и работу с балансом
 
-  const onSendData = useCallback(()=>{
+  const onSendData = useCallback(() => {
     const data = {
-      "0":"011"
-    }
-    tg.sendData(JSON.stringify(data))
-  },[])
+      "0": "011",
+    };
+    tg.sendData(JSON.stringify(data));
+  }, []);
 
   useEffect(() => {
-    tg.onEvent('mainButtonClicked',onSendData)
-    return()=>{
-      tg.offEvent('mainButtonClicked',onSendData)
-    }
+    tg.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      tg.offEvent("mainButtonClicked", onSendData);
+    };
   }, []);
 
   const { tg, user, chatId, onClose, onToggleButton } = useTelegram();
@@ -30,11 +30,11 @@ function App() {
   }, []);
 
   const sendMessageToBot = async () => {
-    console.log(tg)
-      console.log(targetAddress)
-      console.log(`${targetAddress}/api/send-message`)
+    console.log("testClick")
+    // console.log(tg);
+    // console.log(targetAddress);
+    // console.log(`${targetAddress}/api/send-message`);
     try {
-      
       const response = await fetch(`${targetAddress}/api/send-message`, {
         method: "POST",
         headers: {
