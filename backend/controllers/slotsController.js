79e6b043c9dca272452967f4971e5521436e78fb
doctored11 <edit.shultz@yandex.ru>
2024-11-25 14:32:41 +0300

    test api

--- backend/controllers/slotsController.js
+++ backend/controllers/slotsController.js
@@ -1,22 +1,22 @@
 const { sendMessage } = require('../services/botService');
-const { startProjectOptions } = require('./options'); 
-export const spinSlot = async (req, res) => {
-  const { chatId, bet, balance } = req.body;
+const { startProjectOptions } = require('./options');
+const spinSlot = async (req, res) => {
+    const { chatId, bet, balance } = req.body;
 
-  if (!chatId || bet == null || balance == null) {
-    return res.status(400).json({ success: false, error: "chatId, bet и balance обязательны" });
-  }
+    if (!chatId || bet == null || balance == null) {
+        return res.status(400).json({ success: false, error: "chatId, bet и balance обязательны" });
+    }
 
-  try {
+    try {
 
-    const combination = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
-    //логика
-    await sendMessage(chatId, `Ваша комбинация: ${combination.join(' ')}`);
+        const combination = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+        //логика
+        await sendMessage(chatId, `Ваша комбинация: ${combination.join(' ')}`);
 
-    res.status(200).json({ success: true, data: { newBalance: balance - bet, combination } });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
+        res.status(200).json({ success: true, data: { newBalance: balance - bet, combination } });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
 };
 
 module.exports = { spinSlot };
