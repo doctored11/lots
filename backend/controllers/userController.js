03832fb350527e743bc03360db57f024c24952be
doctored11 <edit.shultz@yandex.ru>
2024-11-25 21:33:47 +0300

    hot test sql

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const pool = require('../db');
 
 
 async function getUserByChatId(chatId) {
-    const result = await pool.query('SELECT * FROM user WHERE chat_id = $1', [chatId]);
+    const result = await pool.query('SELECT * FROM "user" WHERE chat_id = $1', [chatId]);
     return result.rows[0];
 }
 
@@ -27,36 +27,36 @@ async function ensureUserExists(chatId, username = 'miniShl3pP3r') {
     return user;
 }
 async function updateUserBalance(chatId, balance) {
-    await pool.query('UPDATE user SET balance = $1 WHERE chat_id = $2', [balance, chatId]);
+    await pool.query('UPDATE "user" SET balance = $1 WHERE chat_id = $2', [balance, chatId]);
 }
 
 
 async function updateusertats(chatId, status, exp, rank) {
     await pool.query(
-        'UPDATE user SET status = $1, exp = $2, rank = $3 WHERE chat_id = $4',
+        'UPDATE "user" SET status = $1, exp = $2, rank = $3 WHERE chat_id = $4',
         [status, exp, rank, chatId]
     );
 }
 
 async function getAllUsers() {
-    const result = await pool.query('SELECT * FROM user');
+    const result = await pool.query('SELECT * FROM "user"');
     return result.rows;
 }
 
 
 async function getUserInfo(chatId) {
-    const result = await pool.query('SELECT * FROM user WHERE chat_id = $1', [chatId]);
+    const result = await pool.query('SELECT * FROM "user" WHERE chat_id = $1', [chatId]);
     return result.rows[0];
 }
 
 
 async function getUserBalance(chatId) {
-    const result = await pool.query('SELECT balance FROM user WHERE chat_id = $1', [chatId]);
+    const result = await pool.query('SELECT balance FROM "user" WHERE chat_id = $1', [chatId]);
     return result.rows[0]?.balance;
 }
 
 async function deleteUser(chatId) {
-    const result = await pool.query('DELETE FROM user WHERE chat_id = $1 RETURNING *', [chatId]);
+    const result = await pool.query('DELETE FROM "user" WHERE chat_id = $1 RETURNING *', [chatId]);
     return result.rows[0];
 }
 
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const cors = require('cors');
 const { bot } = require('./services/botService');
-const slotsRoutes = require('./routes/slotsRoutes'); 
+const slotsRoutes = require('./routes/slotsRoutes');
 const userRoutes = require('./routes/userRoutes');
 const { ensureUserExists } = require('./controllers/userController');
 
-const { startProjectOptions } = require('./options'); 
+const { startProjectOptions } = require('./options');
 
 const app = express();
 app.use(express.json());
@@ -16,28 +16,30 @@ app.use('/api/slots', slotsRoutes);
 app.use('/api/user', userRoutes);
 
 bot.setMyCommands([
-  { command: '/start', description: 'Начальное приветствие' },
-  { command: '/info', description: 'Информация' },
-  { command: '/game', description: 'Тестовая игра' },
+    { command: '/start', description: 'Начальное приветствие' },
+    { command: '/info', description: 'Информация' },
+    { command: '/game', description: 'Тестовая игра' },
 ]);
 
 bot.on('message', async (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
-  try {
-    if (text === '/start') {
-        const username = msg.from.username || `shl${chatId}pp3r`;
-        const user = await ensureUserExists(chatId, username);
-      return bot.sendMessage(chatId, ` ${username}, Пошлеппим? `, startProjectOptions);
+    try {
+        if (text === '/start') {
+
+            const username = msg.from.username || `shl${chatId}pp3r`; 
+            await bot.sendMessage(chatId, username)
+            const user = await ensureUserExists(chatId, username);
+            return bot.sendMessage(chatId, ` ${username}, Пошлеппим? `, startProjectOptions);
+        }
+
+    } catch (e) {
+        bot.sendMessage(chatId, 'Ошибка: ' + e.message);
     }
-    
-  } catch (e) {
-    bot.sendMessage(chatId, 'Ошибка: ' + e.message);
-  }
 });
 
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
