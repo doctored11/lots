260197b9616e1c022583e420a079d64d9932c0b2
doctored11 <edit.shultz@yandex.ru>
2024-11-26 16:37:50 +0300

    hot api test

--- frontend/src/api/useLotsAPI.js
+++ frontend/src/api/useLotsAPI.js
@@ -59,7 +59,7 @@ export function useGameAPI() {
   const changeMachine = async (chatId, balance, machineCost = 50) => {
     console.log('⚙️ Данные для смены автомата:', { chatId, balance, machineCost });
 
-    const body =  JSON.stringify({ chatId, balance, machineCost });
+    const body =  { chatId, balance, machineCost };
     return await request('/api/slots/change-machine', 'POST', body);
 };
 
