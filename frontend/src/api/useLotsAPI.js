e3d1384c5b3f2742d03f2b71f1e2278fb6ddd1af
doctored11 <edit.shultz@yandex.ru>
2024-11-26 16:55:59 +0300

    hot api test

--- frontend/src/api/useLotsAPI.js
+++ frontend/src/api/useLotsAPI.js
@@ -46,7 +46,7 @@ export function useGameAPI() {
 
   
   const spinSlots = async (chatId, bet, balance) => {
-    console.log('⚙️ Данные для спина автомата:', { chatId, balance, machineCost });
+    console.log('⚙️ Данные для спина автомата:', {chatId, bet, balance });
     const body = { chatId, bet, balance };
     return await request('/api/slots/spin', 'POST', body);
   };
