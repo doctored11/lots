29c68ec892fb00733bddb556fae1dd2b35cf7905
doctored11 <edit.shultz@yandex.ru>
2024-11-27 14:24:42 +0300

    fix some logic [hot test]

--- frontend/src/components/changeMashine/ChangeMashine.tsx
+++ frontend/src/components/changeMashine/ChangeMashine.tsx
@@ -1,107 +1,108 @@
 import React, { useContext, useState } from "react";
-import {
-  SlotContext,
-  useSlotContext,
-} from "../slotMashine/slotMashine/SlotContext";
+import { useSlotContext } from "../slotMashine/slotMashine/SlotContext";
 import { getRandomColor } from "../../tools/tools";
 import styles from "./changeMashine.module.css";
 import { PlayerContext } from "../../PlayerContext";
 import { REWARDS } from "../../constants/drumConstants";
-import { useMashineLogic } from "../../components/slotMashine/slotMashine/mashineBody/useMashineLogic";
 
 export function ChangeMashine() {
   const slot = useSlotContext();
   const player = useContext(PlayerContext);
+
   const mashineView = document.getElementById("mashine");
   const shadowView = document.getElementById("shadow");
-  const cssHideAniDuration = 2_000; //мс
-  const cssShowAniDuration = 1_200; //мс
+  const cssHideAniDuration = 2_000; // мс
+  const cssShowAniDuration = 1_200; // мс
   const saveDelta = 100;
 
-  const [isDisabled, setIsDisabled] = useState(false);
-  const { isSpinning,isAnimating,setIsAnimating } = useSlotContext();
+  const { isAnimating, setIsAnimating, isSpinning } = slot;
 
   if (!slot || !player) return null;
-  const handleChangeMashine = async () => {
-    console.log("чендж машины Isspin",isSpinning)
-    if (isDisabled || slot.betInGame > 0 || isSpinning) return;
-    setIsAnimating(true)
 
-    // setIsDisabled(true);
-   
+
+  const startAnimation = () => {
+    setIsAnimating(true);
     shadowView?.classList.add(styles.shadow);
     if (mashineView) {
       setTimeout(() => {
         mashineView.classList.add(styles.mashineHide);
         shadowView?.classList.add(styles.shadowGrow);
       }, saveDelta);
-      // mashineView.classList.add(styles.mashineHide);
-      // shadowView?.classList.add(styles.shadow);
     }
-    let newReel: Array<keyof typeof REWARDS>, newBalance: number; //мы же не можем использовать var (кто вообще придумал так самоограничиваться)
+  };
+
+  const endAnimation = () => {
+    mashineView?.classList.remove(styles.mashineHide);
+    shadowView?.classList.remove(styles.shadowGrow);
+
+    mashineView?.classList.add(styles.mashineShow);
+    shadowView?.classList.add(styles.shadowAppearance);
+
+    setTimeout(() => {
+      mashineView?.classList.remove(styles.mashineShow);
+      shadowView?.classList.remove(styles.shadowAppearance);
+      setIsAnimating(false);
+    }, cssShowAniDuration + saveDelta);
+  };
+
+ 
+  const changeMachineLogic = async () => {
+    let newReel: Array<keyof typeof REWARDS> = [];
+    let newBalance: number = player.balance;
+
     try {
       const response = await slot.getNewMachine(
         player.chatId + "",
         player.balance
       );
-      console.log("ОТвет на смену машины: ", response);
       if (response.success && response.data) {
         newReel = response.data.newReel;
         newBalance = response.data.newBalance;
       } else {
         console.error("Ошибка смены автомата: " + response.error);
         restorePreviousState();
+        return;
       }
     } catch (error) {
       console.error("Ошибка смены автомата:", error);
       restorePreviousState();
-    } finally {
-      // setIsDisabled(false);
-      setIsAnimating(false)
-
+      return;
     }
 
-    setTimeout(() => {
-      mashineView?.classList.remove(styles.mashineHide);
-      shadowView?.classList.remove(styles.shadowGrow);
-
-      mashineView?.classList.add(styles.mashineShow);
-      shadowView?.classList.add(styles.shadowAppearance);
-     
-    }, cssHideAniDuration + 2 * saveDelta);
-
-    setTimeout(async () => {
-      if (newReel.length > 0 && newBalance) {
-        slot.setReel(newReel);
-        slot.setBetStep(10);
-        slot.setLastWin(0);
-        slot.setMaxWin(0);
-        slot.setColor(getRandomColor());
-        player.setBalance(newBalance);
-        console.log("Новая лента автомата:", newReel);
-      } else {
-        restorePreviousState();
-      }
-    }, cssHideAniDuration + saveDelta);
-
-    setTimeout(() => {
-      mashineView?.classList.remove(styles.mashineShow);
-      shadowView?.classList.remove(styles.shadowAppearance);
-    }, cssHideAniDuration + cssShowAniDuration + saveDelta);
+    
+    slot.setReel(newReel);
+    slot.setBetStep(10);
+    slot.setLastWin(0);
+    slot.setMaxWin(0);
+    slot.setColor(getRandomColor());
+    player.setBalance(newBalance);
+    console.log("Новая лента автомата:", newReel);
   };
 
-  function restorePreviousState() {
-    console.warn("🤡 Восстанавленно предыдущее состояние автомата ", slot.reel);
+ 
+  const handleChangeMashine = async () => {
+    if (isAnimating || isSpinning || slot.betInGame > 0) return;
+
+    startAnimation(); 
+    await changeMachineLogic(); 
+    setTimeout(endAnimation, cssHideAniDuration + 2 * saveDelta);
+  };
+
+  
+  const restorePreviousState = () => {
+    console.warn("🤡 Восстановлено предыдущее состояние автомата ", slot.reel);
     slot.setReel(slot.reel);
     slot.setBetStep(slot.betStep);
     slot.setLastWin(slot.lastWin);
     slot.setMaxWin(slot.maxWin);
     slot.setColor(slot.color);
-  }
+    setIsAnimating(false); 
+  };
+
   return (
     <button
       onClick={handleChangeMashine}
-      disabled={isAnimating }
+      disabled={isAnimating || isSpinning}
       className={styles.button}
     >
       {"-Change Slot Machine-"}
